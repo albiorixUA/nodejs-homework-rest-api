3c342b25452e01fb409dee937bb6b274cd7d5d92
unknown <steblynamaxim@gmail.com>
2022-06-18 16:40:35 +0300

    compete homework

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,59 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const id = require("bson-objectid");
 
-const listContacts = async () => {}
+const contactPath = path.join(__dirname, "./contacts.json");
 
-const getContactById = async (contactId) => {}
+const updateData = async (contact) => {
+  await fs.writeFile(contactPath, JSON.stringify(contact, null, 2));
+};
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactPath);
+  return JSON.parse(data);
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const data = await listContacts();
+  const result = data.find((item) => item.id === contactId);
+  if (!result) {
+    return null;
+  }
+  return result;
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  const data = await listContacts();
+  const idx = data.findIndex((iteam) => iteam.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const [removeContact] = data.splice(idx, 1);
+  updateData(data);
+  return removeContact;
+};
+
+const addContact = async (body) => {
+  const data = await listContacts();
+  const newContact = {
+    id: id(),
+    ...body,
+  };
+  data.push(newContact);
+  updateData(data);
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const data = await listContacts();
+  const idx = data.findIndex((iteam) => iteam.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  data[idx] = { id: contactId, ...body };
+  updateData(data);
+  return data[idx];
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +61,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
